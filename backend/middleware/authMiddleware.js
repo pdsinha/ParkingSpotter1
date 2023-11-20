f6a332898de8644e289c8d977be86e8cf24b91bd
Kashir Khan <kashirskhan@gmail.com>
2023-11-19 21:26:24 -0600

    Kashir Test Cases

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -30,4 +30,47 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 })
 
+/*
+// Testing authMiddleware (Kashir)
+// Test functions
+const testProtectWithToken = async () => {
+    const mockReq = {
+        headers: {
+            authorization: 'Bearer validtoken123'
+        }
+    };
+    const mockRes = {};
+    const mockNext = jest.fn();
+
+    // Mock jwt.verify and User.findById
+    jwt.verify = jest.fn().mockReturnValue({ id: 'userId' });
+    User.findById = jest.fn().mockResolvedValue({ id: 'userId', email: 'test@example.com' });
+
+    try {
+        await protect(mockReq, mockRes, mockNext);
+        console.log('Test protect with token: Passed');
+    } catch (error) {
+        console.error('Test protect with token: Failed', error);
+    }
+};
+
+const testProtectWithoutToken = async () => {
+    const mockReq = {
+        headers: {}
+    };
+    const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn()
+    };
+    const mockNext = jest.fn();
+
+    try {
+        await protect(mockReq, mockRes, mockNext);
+        console.error('Test protect without token: Failed - Should not reach this point');
+    } catch (error) {
+        console.log('Test protect without token: Passed');
+    }
+};
+
+*/
 module.exports = { protect }
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,3 @@
-
-
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
     res.status(statusCode)
@@ -9,10 +7,46 @@ const errorHandler = (err, req, res, next) => {
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
 
     })
-
-
 }
 
+/*
+// Testing errorMiddleware.js (Kashir Khan)
+// Test functions
+const testErrorHandlerWithCustomError = () => {
+    const mockReq = {};
+    const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+    };
+    const mockNext = jest.fn();
+    const error = new Error('Error message');
+
+    errorHandler(error, mockReq, mockRes, mockNext);
+
+    console.log('Test errorHandler with custom error:', {
+        statusCalledWith: mockRes.status.mock.calls,
+        jsonCalledWith: mockRes.json.mock.calls
+    });
+};
+
+const testErrorHandlerWithStatusCode = () => {
+    const mockReq = {};
+    const mockRes = {
+        statusCode: 404,
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+    };
+    const mockNext = jest.fn();
+    const error = new Error('Not found');
+
+    errorHandler(error, mockReq, mockRes, mockNext);
+
+    console.log('Test errorHandler with status code:', {
+        statusCalledWith: mockRes.status.mock.calls,
+        jsonCalledWith: mockRes.json.mock.calls
+    });
+};
+*/
 module.exports = {
     errorHandler
 }
