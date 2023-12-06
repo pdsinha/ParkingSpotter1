b35ef424c1844f5e4d26c03816d985a60210e9ff
pdsinha <92610151+pdsinha@users.noreply.github.com>
2023-12-06 13:15:58 -0600

    Update page.js

--- frontend/parking-spotter/src/app/page.js
+++ frontend/parking-spotter/src/app/page.js
@@ -1,12 +1,22 @@
-import Image from 'next/image'
-import Header from "@/components/layout/Header"
+'use client';
+import React from "react";
 import Hero from "@/components/layout/Hero"
+import LoginPage from ".//login/page"
+import {CookiesProvider, useCookies} from "react-cookie";
 
 export default function Home() {
+  const [cookies, setCookie] = useCookies(["user"]);
+
   return (
-   <>
-   
-   <Hero/>
-   </>
-  )
+    <CookiesProvider>
+   <div>
+     {cookies.user &&(
+      <Hero user = {cookies.user} />
+     )}
+     {!cookies.user && (
+      <LoginPage user = {cookies.user}/>
+     )}
+   </div>
+   </CookiesProvider>
+  );
 }
