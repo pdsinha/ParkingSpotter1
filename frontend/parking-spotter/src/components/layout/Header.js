0845d970c20cd414a013002839581aace0730dd4
pdsinha <92610151+pdsinha@users.noreply.github.com>
2023-12-06 13:16:35 -0600

    Update Header.js

--- frontend/parking-spotter/src/components/layout/Header.js
+++ frontend/parking-spotter/src/components/layout/Header.js
@@ -1,19 +1,32 @@
+'use client';
 import Link from "next/link"
-
-export default function Header() {
+import {CookiesProvider, useCookies} from "react-cookie";
+export default function Header(){
+    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
     return (
-        <header className="flex items-center justify-between">
-            <nav className="flex items-center gap-8 text-gray-600 font-semibold">
-                <Link href="/" className="text-title2 font-semibold text-2xl">Parking Spotter</Link>
-                <Link href="/">Home</Link>
-                <Link href="/Map">Map</Link>
-                <Link href="/Report">Reports</Link>
-                <Link href="/about">About</Link>
-            </nav>
-            <nav className="flex items-center gap-8 text-gray-600 font-semibold">
-                <Link href="/login">Login</Link>
-                <Link href="/register" className="bg-title rounded-full text-white px-8 py-2">Register</Link>
-            </nav>
-        </header>
+        <CookiesProvider>
+    <header class= "flex items-center justify-between">
+    
+    <nav class= "flex items-center gap-8 text-gray-600 font-semibold">
+    <Link class= "text-title2 font-semibold text-2xl" href={"/"}>Parking Spotter</Link>
+    <Link href={'/'}>Home</Link>
+    <Link href={'/map'}>Map</Link>
+    <Link href={'/Report'} >Reports</Link>
+    <Link href={'/about'} >About</Link>
+
+    </nav>
+    <nav class="flex items-center gap-8 text-gray-600 font-semibold">
+    {cookies.user &&(
+      <Link href={'/login'}class = "">Logout</Link>
+     )}
+     {!cookies.user && (
+      <Link href={'/login'}class = "">Login</Link>
+     )}
+    
+    <Link href={'/register'} class = "bg-title rounded-full text-white px-8 py-2">Register</Link>
+    
+    </nav>
+   </header>
+   </CookiesProvider>
     );
 }
